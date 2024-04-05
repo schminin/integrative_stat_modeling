771077095c5ba7c8f55b5e95e809fb63511d5ddd
Vincent Wieland <90847066+vwiela@users.noreply.github.com>
2024-04-05 09:44:01 +0200

    Create new test vectors

--- CaseCountDistributions.jl
+++ CaseCountDistributions.jl
@@ -9,7 +9,7 @@ function brownian_reproduction_number(Rt::Float64, variance::Float64)
     return max(0, Rt + rand(Normal(0, variance)))
 end
 
-function InfectionPotential(Y::Vector{T}, ω::Vector{T}) where {N, T<:Real}
+function InfectionPotential(Y::Vector{T}, ω::Vector{T}) where {T<:Real}
     Λ_it = sum(ω.*Y) 
     return Λ_it
 end
@@ -85,6 +85,8 @@ struct CaseCountDistribution{N, T <: Real, K} <: SMultivariateDistribution{N, T,
     - R_t
 
     θ: Vector of parameters with dimension K=2 with the following order
+    - scale of the discretized gamma distribution
+    - shift of the discretized gamma distribution
     """
     function CaseCountDistribution(State::SVector{N, T}, θ::SVector{K, T}) where {N, T <: Real, K}
         ndim = length(State)
@@ -110,7 +112,7 @@ function Random.rand(rng::AbstractRNG, d::CaseCountDistribution{N, T, K})::SVect
 
     # get binomial parameters
     bin_par = ϕ./ (1 .- vcat([0.], cumsum(ϕ))[1:end-1])
-
+    bin_par = min.(bin_par, 1.0)
 
     Λ = InfectionPotential(old_Y[1:end-1], ω) 
 
@@ -121,10 +123,12 @@ function Random.rand(rng::AbstractRNG, d::CaseCountDistribution{N, T, K})::SVect
     new_Y[1], new_A[1] = rand(rng, MixedPoiBin(Λ, old_R, bin_par[1])) #TODO Parameter ordering
 
     # update A_{t-j, j} by binomial
+    println("n", old_Y[1]-old_A[1], ", p", bin_par[2])
     new_A[2] = rand(rng, Binomial(old_Y[1]-old_A[1], bin_par[2]) ) # n of Binomial is: Y_{t-1}-A_{t-1,0}
     for k in 3:d.m_Λ+1
         # e.g. for k=3 -> A_{t-2,2}
         #         -> n of Binomial is: Y_{t-2}-old_sum_A_{t-2} 
+        println("n", old_Y[k]-old_A_sum[k-2], ", p", bin_par[k])
         new_A[k]= rand(rng, Binomial(new_Y[k]-old_A_sum[k-2], bin_par[k])) #TODO Parameter ordering
     end
 
@@ -142,94 +146,3 @@ end
 
 Random.rand(d::CaseCountDistribution) = rand(Random.default_rng(), d)
 
-
-######################################################################
-# Testing Area 
-######################################################################
-
-# test function for initial states
-function check_initial_state(state, m_Λ)
-    Y = state[1:m_Λ+1]
-    A = state[m_Λ+2:2*(m_Λ+1)]
-    A_sum = state[2*m_Λ+3:3*m_Λ+1]
-    R = state[end]
-    if any(A.>Y)
-        println("A cannot be larger than Y")
-        println("Error at: $(.!(A.<=Y))")
-    end
-    if any(A_sum.>Y)
-        println("A_sum cannot be larger than Y")
-        println("Error at: $(.!(A_sum.<=Y))")
-    end
-    if any(A[2:end-1].>A_sum)
-        println("A_{t-i, i} cannot be larger than A_sum_{t}")
-        println("Error at: $(.!(A[2:end-1].<=A_sum))")
-    end
-    if R < 0
-        println("R cannot be negative")
-    end
-end
-
-check_initial_state(SVector(8.0, 7.0, 6.0, 5.0, 2.0, 3.0, 4.0, 1), 2)
-
-
-function test_distribution_update()
-    old_state = SVector(8.0, 7.0, 6.0, 5.0, 2.0, 3.0, 4.0, 1)
-    cc = CaseCountDistribution(old_state, SVector(2.0, 2.0))
-    
-    old_Y = old_state[1:cc.m_Λ+1]
-    old_A = old_state[cc.m_Λ+2:2*(cc.m_Λ+1)]
-    old_A_sum = old_state[2*cc.m_Λ+3:3*cc.m_Λ+1]
-    old_R = old_state[end]
-
-    new_state = rand(cc)
-    new_Y = new_state[1:cc.m_Λ+1]
-    new_A = new_state[cc.m_Λ+2:2*(cc.m_Λ+1)]
-    new_A_sum = new_state[2*cc.m_Λ+3:3*cc.m_Λ+1]
-    new_R = new_state[end]
-
-    # test 1: Update Y is likely correct
-    if ! (new_Y[2:end] == old_Y[1:end-1])
-        println("Error in Y update: Values are not shifted correctly")
-    end
-    if ! (isinteger(new_Y[1]))
-        println("Error in Y update: Y[1] is not an integer")
-    end
-    if (new_Y[2:end] == old_Y[1:end-1]) & isinteger(new_Y[1])
-        println("Update of Y is correct \n")
-    end
-
-    # test 2: Update A is likely correct
-    # subtest a): A[1] is smaller than Y[1]
-    if ! (new_A[1] <= new_Y[1])
-        print("Error in A update: A[1] is larger than Y[1]")
-    end
-    # subtest b): sum of old and new A of one timepoint of infection are smaller than corresponding Y
-    if !all(new_A[2:end] + old_A[1:end-1] .<= new_Y[2:end])
-        println("Error in A update: sum of two A's is larger than corresponding Y")
-        println("Error (1) at: $(.!(new_A[2:end] + old_A[1:end-1] .<= old_Y[1:end-1]))")
-    end
-    println("Update of A looks good")
-    
-    # test 3: Update of A_sum is likely correct
-    if !(new_A_sum[1] == old_A[1] + new_A[2])
-        print("Error in A_sum update: A_sum[1] is not correct")
-    end
-    println("Update of sum A looks good")
-
-end
-
-# perform 10 tests
-[test_distribution_update() for i in 1:10]
-
-
-ytest = SVector(1.0, 2.0, 3.0)
-
-old_test = ytest[SVector(Array(range(1,3))...)]
-
-new_test = similar(old_test)
-
-om, pi = CaseCountParameterMapping(SVector(2.0, 2.0), 8)
-om
-
-pi./ (1 .- vcat([0.], cumsum(pi))[1:end-1])
---
+++ unit_tests.jl
@@ -0,0 +1,101 @@
+
+######################################################################
+# Testing Area for the CaseCountDistribution and CaseCountParameterMapping
+######################################################################
+
+include("CaseCountDistributions.jl")
+
+# test function for initial states
+function check_initial_state(state, m_Λ)
+    Y = state[1:m_Λ+1]
+    A = state[m_Λ+2:2*(m_Λ+1)]
+    A_sum = state[2*m_Λ+3:3*m_Λ+1]
+    R = state[end]
+    if any(A.>Y)
+        println("A cannot be larger than Y")
+        println("Error at: $(.!(A.<=Y))")
+    end
+    if any(A_sum.>Y[2:end-1])
+        println("A_sum cannot be larger than Y")
+        println("Error at: $(.!(A_sum.<=Y[2:end-1]))")
+    end
+    if any(A[2:end-1].>A_sum)
+        println("A_{t-i, i} cannot be larger than A_sum_{t}")
+        println("Error at: $(.!(A[2:end-1].<=A_sum))")
+    end
+    if R < 0
+        println("R cannot be negative")
+    end
+end
+
+old_state_2 = SVector(8.0, 7.0, 6.0, 5.0, 2.0, 3.0, 4.0, 1)
+check_initial_state(old_state_2, 2)
+
+
+old_state_4 = SVector(4.0, 8.0, 7.0, 15.0, 3.0, 2.0, 3.0, 6.0, 2.0, 2.0, 7.0, 6.0, 2.0, 1.0)
+check_initial_state(old_state_4, 4)
+
+
+function test_distribution_update(old_state)
+    cc = CaseCountDistribution(old_state, SVector(2.0, 2.0))
+    
+    old_Y = old_state[1:cc.m_Λ+1]
+    old_A = old_state[cc.m_Λ+2:2*(cc.m_Λ+1)]
+    old_A_sum = old_state[2*cc.m_Λ+3:3*cc.m_Λ+1]
+    old_R = old_state[end]
+
+    new_state = rand(cc)
+    new_Y = new_state[1:cc.m_Λ+1]
+    new_A = new_state[cc.m_Λ+2:2*(cc.m_Λ+1)]
+    new_A_sum = new_state[2*cc.m_Λ+3:3*cc.m_Λ+1]
+    new_R = new_state[end]
+
+    # test 1: Update Y is likely correct
+    if ! (new_Y[2:end] == old_Y[1:end-1])
+        println("Error in Y update: Values are not shifted correctly")
+    end
+    if ! (isinteger(new_Y[1]))
+        println("Error in Y update: Y[1] is not an integer")
+    end
+    if (new_Y[2:end] == old_Y[1:end-1]) & isinteger(new_Y[1])
+        println("Update of Y is correct \n")
+    end
+
+    # test 2: Update A is likely correct
+    # subtest a): A[1] is smaller than Y[1]
+    if ! (new_A[1] <= new_Y[1])
+        print("Error in A update: A[1] is larger than Y[1]")
+    end
+    # subtest b): sum of old and new A of one timepoint of infection are smaller than corresponding Y
+    if !all(new_A[2:end] + old_A[1:end-1] .<= new_Y[2:end])
+        println("Error in A update: sum of two A's is larger than corresponding Y")
+        println("Error (1) at: $(.!(new_A[2:end] + old_A[1:end-1] .<= old_Y[1:end-1]))")
+    end
+    println("Update of A looks good")
+    
+    # test 3: Update of A_sum is likely correct
+    if !(new_A_sum[1] == old_A[1] + new_A[2])
+        print("Error in A_sum update: A_sum[1] is not correct")
+    end
+    println("Update of sum A looks good")
+
+end
+
+# perform 10 tests
+test_distribution_update(old_state_4)
+
+
+ytest = SVector(1.0, 2.0, 3.0)
+
+old_test = ytest[SVector(Array(range(1,3))...)]
+
+new_test = similar(old_test)
+
+om, pi = CaseCountParameterMapping(SVector(2.0, 2.0), 8)
+om
+
+pi./ (1 .- vcat([0.], cumsum(pi))[1:end-1])
+
+bin_par = [0.8, 1.1]
+bin_par = min.(bin_par, 1.0)
+
