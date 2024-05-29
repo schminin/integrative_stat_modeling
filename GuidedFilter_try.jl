12ecfbed89c6bc3620f92633ffd837da509a0deb
Vincent Wieland <90847066+vwiela@users.noreply.github.com>
2024-05-29 14:45:02 +0200

    Work on Guided filter in simple Linear-Gauss model example.

--- GuidedFilter_try.jl
+++ GuidedFilter_try.jl
@@ -11,6 +11,9 @@ using Plots
 using StatsFuns
 using StatsPlots
 using DataFrames
+using AdvancedMH
+using MCMCChains
+using BenchmarkTools
 
 
 
@@ -23,14 +26,13 @@ struct LinearGaussBF <: StateSpaceModel{Float64, Float64}
 end
 LinearGaussBF(; rho=0.9, sigmaX=1.0, sigmaY=0.2, sigma0=sigmaX/sqrt(1-rho^2)) = LinearGaussBF(rho, sigmaX, sigmaY, sigma0)
 
-# Particles.parameter_type(::LinearGaussBF) = Vector{Float64}
-# Particles.parameter_template(::LinearGaussBF) = Float64[0.9, 1.0, 0.2]
-# Particles.isparameter(::LinearGaussBF, θ) = isa(θ, Vector{Float64}) && length(θ) == 3
+Particles.ssm_PX0(ssm::LinearGaussBF, θ::AbstractVector{<:Real}) = Normal(0.0, θ[2]/sqrt(1-θ[1]^2))
+Particles.ssm_PX(ssm::LinearGaussBF, θ::AbstractVector{<:Real}, t::Integer, xp::Real) = Normal(θ[1]*xp, θ[2])
+Particles.ssm_PY(ssm::LinearGaussBF, θ::AbstractVector{<:Real}, t::Integer, x::Real) = Normal(x, θ[3])
 
-Particles.ssm_PX0(ssm::LinearGaussBF, ::Nothing) = Normal(0.0, ssm.sigma0)
-Particles.ssm_PX(ssm::LinearGaussBF, ::Nothing, t::Integer, xp::Real) = Normal(ssm.rho*xp, ssm.sigmaX)
-Particles.ssm_PY(ssm::LinearGaussBF, ::Nothing, t::Integer, x::Real) = Normal(x, ssm.sigmaY)
-
+Particles.parameter_type(::LinearGaussBF) = Vector{Float64} # TODO allow different parameter type: add a function that only checks type isparametertypecorrect or something
+Particles.parameter_template(::LinearGaussBF) = Float64[0.9, 1.0, 0.2]
+Particles.isparameter(::LinearGaussBF, θ) = isa(θ, Vector{Float64}) && length(θ) == 3
 
 # define the linear Gauss model with proposal distributions
 struct LinearGaussGuided <: StateSpaceModel{Float64, Float64}
@@ -41,13 +43,13 @@ struct LinearGaussGuided <: StateSpaceModel{Float64, Float64}
 end
 LinearGaussGuided(; rho=0.9, sigmaX=1.0, sigmaY=0.2, sigma0=sigmaX/sqrt(1-rho^2)) = LinearGaussGuided(rho, sigmaX, sigmaY, sigma0)
 
-# Particles.parameter_type(::LinearGaussGuided) = Vector{Float64}
-# Particles.parameter_template(::LinearGaussGuided) = Float64[0.9, 1.0, 0.2]
-# Particles.isparameter(::LinearGaussGuided, θ) = isa(θ, Vector{Float64}) && length(θ) == 3
+Particles.parameter_type(::LinearGaussGuided) = Vector{Float64} # TODO allow different parameter type: add a function that only checks type isparametertypecorrect or something
+Particles.parameter_template(::LinearGaussGuided) = Float64[0.9, 1.0, 0.2]
+Particles.isparameter(::LinearGaussGuided, θ) = isa(θ, Vector{Float64}) && length(θ) == 3
 
-Particles.ssm_PX0(ssm::LinearGaussGuided, ::Nothing) = Normal(0.0, ssm.sigma0)
-Particles.ssm_PX(ssm::LinearGaussGuided, ::Nothing, t::Integer, xp::Real) = Normal(ssm.rho*xp, ssm.sigmaX)
-Particles.ssm_PY(ssm::LinearGaussGuided, ::Nothing, t::Integer, x::Real) = Normal(x, ssm.sigmaY)
+Particles.ssm_PX0(ssm::LinearGaussGuided, θ::AbstractVector{<:Real}) = Normal(0.0, θ[2]/sqrt(1-θ[1]^2))
+Particles.ssm_PX(ssm::LinearGaussGuided, θ::AbstractVector{<:Real}, t::Integer, xp::Real) = Normal(θ[1]*xp, θ[2])
+Particles.ssm_PY(ssm::LinearGaussGuided, θ::AbstractVector{<:Real}, t::Integer, x::Real) = Normal(x, θ[3])
 
 has_proposal(::LinearGaussGuided) = static(true)
 proposal_parameters(::LinearGaussGuided) = nothing
@@ -69,21 +71,23 @@ end
 
 # define StateSpaceModels
 ssm_bf = LinearGaussBF(sigmaX=1., sigmaY=2.0, rho=.9)
-x_bf, data_bf = rand(ssm_bf, 100)
-bf = BootstrapFilter(ssm_bf, data_bf)
+
+T = 1000
+θ_true = Particles.parameter_template(ssm_bf)
+x_bf, data = rand(ssm_bf, θ_true, T)
+# hopefully this is a valid data creation
+bf = BootstrapFilter(ssm_bf, data)
 
 ssm_guided = LinearGaussGuided(sigmaX=1., sigmaY=2.0, rho=.9)
-x_guided, data_guided = rand(ssm_guided, 100)
-gf = BootstrapFilter(ssm_guided, data_guided)
+x_guided, data_guided = rand(ssm_guided, θ_true, 1000)
+gf = BootstrapFilter(ssm_guided, data)
 
-# plot data against each other to compare
-sim_bf = repeat([rand(ssm_bf, 100)[2] for i in 1:1000])
-sim_guided = repeat([rand(ssm_guided, 100)[2] for i in 1:1000])
+# plot data
 
-plot(mean(sim_bf), label="Data BF")
-plot!(mean(sim_guided), label="Data Guided")
+plt = scatter(1:T, x_bf, mode="markers", name="state")
+scatter!(plt, 1:T, data, mode="markers", name="observation")
 
-# not completely agreeing but should be the same process, all centered at 0.
+
 
 # run Bf vs. Guided Filter and plot violin Plots
 function logp_vs_nparticles(ssm::StateSpaceModel, data, nparticles::AbstractVector{<:Integer}, theta; nruns::Integer=10, kwargs...)
@@ -132,12 +136,101 @@ end
 nruns = 200
 nparticles = [10, 20, 50, 100, 200, 500, 1000]
 
-plt_df_bf = logp_vs_nparticles(ssm_bf, data_bf, nparticles, nothing; nruns=nruns)
-plt_df_guided = logp_vs_nparticles(ssm_guided, data_guided, nparticles, nothing; nruns=nruns)
+plt_df_bf = logp_vs_nparticles(ssm_bf, data, nparticles, nothing; nruns=nruns)
+plt_df_guided = logp_vs_nparticles(ssm_guided, data, nparticles, nothing; nruns=nruns)
 
 var_bf = [var(plt_df_bf.logp[plt_df_bf.nparticles .== "$n"]) for n in nparticles]
 var_guided = [var(plt_df_guided.logp[plt_df_guided.nparticles .== "$n"]) for n in nparticles]
 
 findall(var_bf .> var_guided)
 # variances are similar or even better for the Bootstrapfilter, so how to test whether guided really works better.
+
+# perform inference with both filters
+function run_smc_jl(fk::FeynmanKacModel{T}, N::Integer) where {T}
+    f = SMC(
+        fk, Particles.parameter_template(fk), N,
+        
+        ParticleHistoryLength(FullHistory()),
+        
+        (mean_and_var=RunningSummary(MeanAndVariance(), FullHistory()), ),
+        #(mean_and_var=RunningSummary(MeanAndVariance(), StaticFiniteHistory{3}()), ),
+        #(mean_and_var=MeanAndVariance(), ),
+        #NamedTuple(),
+        
+        AdaptiveResampling(SystematicResampling(), 0.5),
+    )
+    offlinefilter!(f)
+    return f
+end
+
+nparticles = 1000
+bootstrap = @btime run_smc_jl(bf, nparticles)
+
+guided = @btime run_smc_jl(gf, nparticles)
+
+bootstrap.history_pf.logCnorm[end], guided.history_pf.logCnorm[end]
+# log-likelihood and speed are the same basically.
+
+# Offline summary computation
+let step = END-2
+    offline_value = Particles.compute_summary(bootstrap, OfflineSummary(MeanAndVariance()), step)
+    if hasproperty(bootstrap.history_run, :mean_and_var)
+        @assert offline_value == bootstrap.history_run.mean_and_var[step]
+    end
+    offline_value
+end
+
+let step = END-2
+    offline_value = Particles.compute_summary(guided, OfflineSummary(MeanAndVariance()), step)
+    if hasproperty(guided.history_run, :mean_and_var)
+        @assert offline_value == guided.history_run.mean_and_var[step]
+    end
+    offline_value
+end
+
+if hasproperty(bootstrap.history_run, :mean_and_var)
+    bootstrap_means = getproperty.(bootstrap.history_run.mean_and_var, :mean)
+    bootstrap_vars = getproperty.(bootstrap.history_run.mean_and_var, :var)
+end;
+if hasproperty(guided.history_run, :mean_and_var)
+    guided_means = getproperty.(guided.history_run.mean_and_var, :mean)
+    guided_vars = getproperty.(guided.history_run.mean_and_var, :var)
+end;
+
+# plot the means and variances
+
+plot((1:T)[end-length(bootstrap_means)+1:end], bootstrap_means, mode="lines", line_color="blue", name="particle (jl) filter mean")
+plot!((1:T)[end-length(bootstrap_means)+1:end], bootstrap_means.+sqrt.(bootstrap_vars), mode="lines", line_color="lightblue", name="particle (jl) filter mean + 1sd")
+plot!((1:T)[end-length(bootstrap_means)+1:end], bootstrap_means.-sqrt.(bootstrap_vars), mode="lines", line_color="lightblue", name="particle (jl) filter mean - 1sd")
+plot!((1:T)[end-length(guided_means)+1:end], guided_means, mode="lines", line_color="red", name="particle (jl) filter mean")
+plot!((1:T)[end-length(guided_means)+1:end], guided_means.+sqrt.(guided_vars), mode="lines", line_color="orange", name="particle (jl) filter mean + 1sd")
+plot!((1:T)[end-length(guided_means)+1:end], guided_means.-sqrt.(guided_vars), mode="lines", line_color="orange", name="particle (jl) filter mean - 1sd")
+# scatter!(1:T, x_bf, mode="markers", name="state")
+scatter!(1:T, data, mode="markers", name="observation", alpha=0.5)
+
+# Both filters produce similar trajectories, so we should look at inference I guess
+
+nparticles = 1000
+true_parameter = [ssm_bf.rho, ssm_bf.sigmaX, ssm_bf.sigmaY]
+initial_parameter = [1.0, 0.9, 0.15]
+parameter_names = ["rho", "sigmaX", "sigmaY"]
+
+llh_bf = LogLikelihood_NoGradient(ssm_bf, data, nparticles=nparticles)
+model_bf = DensityModel(llh_bf)
+llh_guided = LogLikelihood_NoGradient(ssm_guided, data, nparticles=nparticles)
+model_guided = DensityModel(llh_guided)
+
+sampler = RWMH([Normal(0.0, 0.05), Normal(0.0, 0.05), Normal(0.0, 0.05)])
+
+chain_bf = sample(model_bf, sampler, 100; init_params=initial_parameter, param_names=parameter_names, chain_type=Chains)
+
+chain_guided = sample(model_guided, sampler, 100; init_params=initial_parameter, param_names=parameter_names, chain_type=Chains)
+# noch probleme mit negativen Parametern irgendwie....
+# Wie war das nochmal mit Parameter bounds implementieren?
+
+
+reset!(llh_bf.pf, [1.0, 1.0, 1.0])
+
+test_par = [1.0, 1.0, 1.0]
+llh_bf(test_par)
 
